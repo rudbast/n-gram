7299aca881c78d603ad081a5fc79b1cdfe175a53
Rudolf <rudolf_bast@live.com>
2016-04-04 19:20:14 +0700

    Update .gitignore & perl test file

--- src/test.pl
+++ src/test.pl
@@ -4,23 +4,39 @@ use strict;
 use warnings;
 
 use Ngram;
+use String::KeyboardDistanceXS qw(:all);
+use Text::Levenshtein qw(fastdistance);
 
 main();
 
 sub main {
     my %ngrams;
     my $inputFile = $ARGV[0];
+    my %list = ();
 
     open(INPUT, $inputFile) or die "open file error.";
     while(<INPUT>) {
         chomp(my $line = $_);
         # start ngram word splicing
         %ngrams = trigram(\%ngrams, $line);
+        # foreach my $word (split /\s/, $line) {
+        #     # if (fastdistance($word, "sate") <= 2) {
+        #     #     $list{ $word } = 1;
+        #     # }
+        #     # print "$word " . fastdistance($word, "suka") . "\n";
+        #     # $list{ $word } = fastdistance($word, "suka");
+        # }
     }
     close(INPUT);
+
+    # foreach my $word (sort keys %list) {
+    #     print "$word\t$list{ $word }\n";
+    # }
 
     # print result
     foreach my $word (sort {$ngrams{ $b } <=> $ngrams{ $a } or $a cmp $b} keys %ngrams) {
         printf("%25s : %2d\n", $word, $ngrams{ $word });
     }
+    # print qwertyKeyboardDistance('bitu', 'bito') . "\n";
+
 }
