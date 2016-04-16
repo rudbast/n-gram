4fa07ff8d6f23a32fedf03545770aef6e69b38e8
Rudolf <rudolf_bast@live.com>
2016-04-16 15:59:07 +0700

    Add new method (extra step on cleaning content) and improve cleaning

--- src/util/Helper.js
+++ src/util/Helper.js
@@ -3,14 +3,26 @@
 /**
  * Clean article content.
  *
- * @param  {string} content Content to be clean
+ * @param  {string} content Content to be cleaned
  * @return {string}         Cleaned content
  */
-function clean(content) {
+function cleanInitial(content) {
     content = content.toLowerCase();
-    content = content.replace(/("|\*|'|\/)/g, '');
-    content = content.replace(/(:|\(|\))/g, ',');
-    content = content.replace(/(\-)/g, ' ');
+    content = content.replace(/([:;()!?<>\%\$\/"'\*\d{}^=|]|\s\-\s)/g, ',');
+    content = content.replace(/,+/g, ', ');
+    content = content.replace(/\s+/g, ' ');
+    return content;
+}
+
+/**
+ * Extra step on cleaning content.
+ *
+ * @param  {string} content Content to be cleaned
+ * @return {string}         Cleaned content
+ */
+function cleanExtra(content) {
+    content = content.replace(/\./g, ' ');
+    content = content.replace(/\-/g, ' ');
     content = content.replace(/\s+/g, ' ');
     return content;
 }
@@ -26,11 +38,12 @@ function splitToSentence(text) {
     require('shelljs/global');
     var scriptFullPath = __dirname + '/textment.php';
     var scriptProcess  = exec('php ' + scriptFullPath + ' "' + text + '"', {silent: true});
-    var sentences      = JSON.parse(scriptProcess.stdout);
+    var sentences      = Array.from(JSON.parse(scriptProcess.stdout));
     return sentences;
 }
 
 module.exports = {
-    clean,
-    splitToSentence,
+    cleanInitial,
+    cleanExtra,
+    splitToSentence
 };
