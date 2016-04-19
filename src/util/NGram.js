d945f5393847fead8c5e0b47978ca0a8d07b9602
Rudolf <rudolf_bast@live.com>
2016-04-20 01:20:34 +0700

    Add new method to return all N (1,2,3) words partition efficiently

--- src/util/NGram.js
+++ src/util/NGram.js
@@ -66,8 +66,54 @@ function triSplit(text) {
     return trigrams;
 }
 
+/**
+ * Split a text into 1,2,3-gram collection of words.
+ *
+ * @param  {string} text Text to be split
+ * @return {object}      Split words in a object (for each n-gram)
+ */
+function tripleNSplit(text) {
+    var words  = uniSplit(text),
+        ngrams = {
+            unigrams: [],
+            bigrams: [],
+            trigrams: []
+        };
+
+    if (words.length < 2) {
+        ngrams.unigrams = words;
+        return ngrams;
+    }
+
+    var current  = words.shift(),
+        next     = words.shift(),
+        last;
+
+    ngrams.unigrams.push(current);
+    ngrams.unigrams.push(next);
+
+    while (words.length > 0) {
+        last = words.shift();
+
+        var unigram = last,
+            bigram  = `${current} ${next}`,
+            trigram = `${current} ${next} ${last}`;
+
+        ngrams.unigrams.push(unigram);
+        ngrams.bigrams.push(bigram);
+        ngrams.trigrams.push(trigram);
+
+        current = next;
+        next    = last;
+    }
+    ngrams.bigrams.push(`${current} ${next}`);
+
+    return ngrams;
+}
+
 module.exports = {
     uniSplit,
     biSplit,
-    triSplit
+    triSplit,
+    tripleNSplit
 }
