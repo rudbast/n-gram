690f205752a72cb84b428796e2865cc1d56ad933
Rudolf <rudolf_bast@live.com>
2016-05-08 01:06:11 +0700

    Add files commit from previous changes (split main.js' content)

---
+++ src/web/public/assets/js/index.js
@@ -0,0 +1,43 @@
+function formSubmitSuccess(response) {
+    var corrections = $.map(response.corrections, function (probability, sentence) {
+        return [{
+            sentence: sentence,
+            probability: probability
+        }];
+    });
+
+    // Sort customly.
+    corrections.sort(function (a, b) {
+        // Sort probability descendingly.
+        if (a.probability > b.probability) {
+            return -1;
+        } else if (a.probability < b.probability) {
+            return 1;
+        } else {
+            // Sort sentence ascendingly.
+            if (a.sentence < b.sentence) {
+                return -1;
+            } else if (a.sentence > b.sentence) {
+                return 1;
+            } else {
+                return 0;
+            }
+        }
+    });
+
+    corrections.forEach(function (correction) {
+        var sentence    = correction.sentence,
+            probability = correction.probability,
+            childNode   =
+                '<div class="tooltipped" data-position="bottom" data-delay="50" data-tooltip="Probability: ' + probability + '">\
+                    <span class="correction-item">' + sentence + '</span>\
+                </div>';
+        resultListContainer.append('<li class="collection-item">' + childNode + '</li>');
+    });
+
+    // Initialize tooltip.
+    $('.tooltipped').tooltip({delay: 50});
+    $('.correction-item').click(function () {
+        $('#sentence').val($(this).text());
+    });
+}
